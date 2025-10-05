ec9aaaffd4f8b4036feab40cf1839de78cb203d4
Akash-Verma96 <av3764909@gmail.com>
2025-10-05 15:08:21 +0530

    chunk bug

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-export default {
+import { defineConfig } from "vite"
+
+export default{
     root: 'src/',
     publicDir: '../static/',
     base: './',
+    build: {
+    chunkSizeWarningLimit: 2000, 
+  },
 }
+
+
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   build: {
+//     chunkSizeWarningLimit: 2000, 
+//   },
+// });
